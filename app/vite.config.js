be1b54afbac33ad3f57ba9e18d87269905df58e0
Yelsaw <47115872+yelsaw@users.noreply.github.com>
2022-11-16 17:43:45 -0600

    add leading slash to vite config base

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  base: "doc-search/",
+  base: "/doc-search/",
   plugins: [svelte()]
 })
